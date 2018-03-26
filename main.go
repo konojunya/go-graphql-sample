7e3514501155b438deeb1d0114360c3b72a72950
konojunya <junya03dance@gmail.com>
2018-03-26 14:28:05 +0900

    complete

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/graphql-go/graphql"
+)
+
+var q graphql.ObjectConfig = graphql.ObjectConfig{
+	Name: "query",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type:    graphql.ID,
+			Resolve: resolveID,
+		},
+		"name": &graphql.Field{
+			Type:    graphql.String,
+			Resolve: resolveName,
+		},
+	},
+}
+
+var schemaConfig graphql.SchemaConfig = graphql.SchemaConfig{
+	Query: graphql.NewObject(q),
+}
+
+var schema, _ = graphql.NewSchema(schemaConfig)
+
+func resolveID(p graphql.ResolveParams) (interface{}, error) {
+	return 1, nil
+}
+
+func resolveName(p graphql.ResolveParams) (interface{}, error) {
+	return "konojunya", nil
+}
+
+func executeQuery(query string, schema graphql.Schema) *graphql.Result {
+	r := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	})
+	if len(r.Errors) > 0 {
+		log.Fatal("you has errors")
+	}
+
+	j, _ := json.Marshal(r)
+	fmt.Printf("%s \n", j)
+
+	return r
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	bufBody := new(bytes.Buffer)
+	bufBody.ReadFrom(r.Body)
+	query := bufBody.String()
+	result := executeQuery(query, schema)
+	json.NewEncoder(w).Encode(result)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
